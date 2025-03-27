db7a1d7eae341cd68c719725a48dde0abbe85e5f
Mohamed Ezat <ezat6518@gmail.com>
2025-03-27 09:49:15 +0200

    new project

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import HeroSection from "./home-sections/HeroSection";
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection/>
+    </>
+  );
+}
